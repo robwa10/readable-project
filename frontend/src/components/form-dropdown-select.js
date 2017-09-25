487ea20036580ac41c9de25e6495e3cda7f107dd
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-09-25 06:06:16 -0500

    Add onChange prop to select

--- frontend/src/components/form-dropdown-select.js
+++ frontend/src/components/form-dropdown-select.js
@@ -4,7 +4,7 @@ const DropDown = (props) => {
   return (
     <div className="form-group">
       <label style={props.labelStyle}>{props.label}</label>
-      <select className="form-control" id={props.id}>
+      <select className="form-control" id={props.id} onChange={props.onChange}>
         {props.options.map((option, index) => (
           <option key={option[index]}>{option}</option>
         ))}
