ace90d605c48fe9f0bb7fb01f898929b4d8e6ed2
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-09-29 22:00:05 -0500

    Link container instead of title.

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 const Header = (props) => {
   return (
     <div className="header container-fluid">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
+      <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
               <h1 className="text-uppercase text-center">anonymous posting board</h1>
-            </Link>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h4 className="text-muted text-center">Sticks and stones may break my bones...</h4>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <h4 className="text-muted text-center">Sticks and stones may break my bones...</h4>
-          </div>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
