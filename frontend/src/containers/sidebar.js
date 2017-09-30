912f8c3bf82bb4832ddd26b17a55b7ddc66d2ed4
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-09-30 13:40:21 -0500

    Remove api call and call action creator instead.

--- frontend/src/containers/sidebar.js
+++ frontend/src/containers/sidebar.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { recieveCategories } from '../actions';
-import * as api from '../API_Calls';
+import { getPostsOrCategories } from '../actions';
 import { Link } from 'react-router-dom';
 
 class Sidebar extends Component {
   componentDidMount() {
-    this.fetchCategories();
-  }
-
-  fetchCategories() {
-    api.getPostsOrCategories('categories').then((data) => {
-      this.props.recieveCategories(data); // Pass to categories-actions
-    })
+    this.props.getPostsOrCategories('categories');
   }
 
   render() {
-  const categories = this.props.categories
   return (
     <div className="col-md-3">
       <div className="card">
@@ -24,15 +16,15 @@ class Sidebar extends Component {
           <div>
             <h4>Filter</h4>
             <div className="row">
-            {categories.map(cat => {
-              const c = cat[0].charAt(0).toUpperCase() + cat[0].slice(1);
+            {this.props.categories.map(cat => {
+              const c = cat[0];
               return (
                 <Link
                   key={c}
                   to={`/${c}`}
                   role="button"
                   className="p-2 col-12 col-md-6"
-                >{c}</Link>
+                >{c.charAt(0).toUpperCase() + c.slice(1)}</Link>
                 )
               })}
               <Link
@@ -53,4 +45,4 @@ const mapStateToProps = (state) => ({
   categories: state.categories,
 })
 
-export default connect(mapStateToProps, { recieveCategories })(Sidebar);
+export default connect(mapStateToProps, { getPostsOrCategories })(Sidebar);
