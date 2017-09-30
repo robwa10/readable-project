5fa06ad4aad69cbb79074107d8074e13dff340be
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-09-29 20:24:41 -0500

    Change from class to ES6 fat arrow function.

--- frontend/src/components/home-page.js
+++ frontend/src/components/home-page.js
@@ -5,26 +5,18 @@ import Sidebar from '../containers/sidebar';
 import Post from '../components/post';
 import Fab from '../components/fab';
 
-class HomePage extends Component {
-
-  render() {
-
-    return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <Sidebar />
-            <Switch>
-              <Route path='/:filter?' component={VisiblePosts}/>
-              <Route path='/post/:id?' component={Post} />
-            </Switch>
-          </div>
-        </div>
-        <Fab />
+const HomePage = () => (
+  <div>
+    <div className="container">
+      <div className="row">
+        <Sidebar />
+        <Switch>
+          <Route path='/:filter?' component={VisiblePosts}/>
+          <Route path='/post/:id' component={Post} />
+        </Switch>
       </div>
-    )
-  }
-}
-
-
+    </div>
+    <Fab />
+  </div>
+);
 export default HomePage;
