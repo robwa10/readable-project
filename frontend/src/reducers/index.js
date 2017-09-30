70730f1ad9a12304d7c3367f601ec87d0eb98b24
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-09-29 21:27:10 -0500

    Combine single-post-reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import allPostsReducer from './all-posts-reducer';
 import allCategoriesReducer from './all-categories-reducer';
+import singlePostReducer from './single-post-reducer';
 import filterOptions from './filter-options-reducer';
 
 const rootReducer = combineReducers({
   posts: allPostsReducer,
   categories: allCategoriesReducer,
   options: filterOptions,
+  post: singlePostReducer,
 });
 
 export default rootReducer;
