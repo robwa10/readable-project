1aecfe21369c44dc80be21c63cd640d978a32ac6
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-02 08:21:52 -0500

    Refactor API calls to work with redux-thunk and fetch

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 export const RECIEVE_CATEGORIES = 'RECIEVE_CATEGORIES';
 export const RECIEVE_POSTS = 'RECIEVE_POSTS';
 export const RECIEVE_POST = 'RECIEVE_POST';
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
 
-//---------- API Action Creators
-
+//---------- GET API Action Creators
 const BASE_URL = "http://localhost:3001";
 
 // Generate unique token for headers
@@ -18,27 +19,43 @@ const headers = {
   'Authorization': token,
 }
 
-// Base axios call
-const getData = (path) => (axios.get(`${BASE_URL}/${path}`, { headers }));
-
-// Get all posts, all categories or posts filtered by category type
-export const getPostsOrCategories = (filter) => {
-  let action = RECIEVE_POSTS;
-  let request;
-  if (filter === 'posts') {
-    request = getData(filter);
-  } else if (filter === 'categories') {
-    request = getData(filter);
-    action = RECIEVE_CATEGORIES;
-  } else {
-    request = getData(`${filter}/posts`);
+export const getPosts = (filter) => {
+  return function(dispatch) {
+    return fetch(`${BASE_URL}/${filter}`, { headers })
+      .then(resp => resp.json())
+      .then(json => dispatch({ type: RECIEVE_POSTS, json}))
   }
-  return ({ type: action, payload: request})
 }
 
-// Get a single post
-export const getSinglePost = (filter) => (
-  {type: RECIEVE_POST, payload: getData(`posts/${filter}`)}
-);
+export const getPostsByCategory = (filter) => {
+  return function(dispatch) {
+    return fetch(`${BASE_URL}/${filter}/posts`, { headers })
+      .then(resp => resp.json())
+      .then(json => dispatch({ type: RECIEVE_POSTS, json}))
+  }
+}
 
-//---------- End API Action Creators
+export const getSinglePost = (filter) => {
+  return function(dispatch) {
+    return fetch(`${BASE_URL}/posts/${filter}`, { headers })
+      .then(resp => resp.json())
+      .then(json => dispatch({type: RECIEVE_POST, json}))
+  }
+}
+
+export const getCategories = () => {
+  return function(dispatch) {
+    return fetch(`${BASE_URL}/categories`, { headers })
+      .then(resp => resp.json())
+      .then(json => dispatch({ type: RECIEVE_CATEGORIES, json}))
+  }
+}
+
+
+//---------- POST API Action Creators
+export const postVote = (id, option) => {
+  return function(dispatch) {
+    return axios.post(`${BASE_URL}/posts/${id}`,{ option },{ headers })
+    .then(json => dispatch({ type: INCREMENT, json }));
+  }
+}
