6d4aebab82a30cb6d875302a3d540edc1328acd2
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-03 19:53:03 -0500

    Call new API's

--- frontend/src/containers/VisiblePosts.js
+++ frontend/src/containers/VisiblePosts.js
@@ -3,33 +3,37 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Sidebar from '../containers/sidebar';
 import PostPreview from '../components/post-preview';
+import VoteButton from '../containers/vote-button';
 
 class VisiblePosts extends Component {
   componentDidMount() {
-    this.fetchData();
+    this.props.getPosts('posts');
   }
 
-  componentWillUpdate(prevProps) {
-    if (this.props.filter !== prevProps.filter || !undefined) {
-      this.fetchData();
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      if (this.props.filter === 'posts' || undefined) {
+        this.props.getPosts('posts');
+      } else {
+        this.props.getPostsByCategory(this.props.filter);
+      }
     }
   }
 
   fetchData() {
     const { filter, getPosts } = this.props;
-    this.props.getPostsOrCategories(filter);
+    getPosts(filter);
   }
 
   render() {
-    const posts = this.props.posts;
     return (
       <div className="row">
         <Sidebar />
         <div className="col-md-9 mt-5 mt-md-auto" id="posts-list">
           <div className="card">
             <div className="card-body">
-              {posts
-                ? posts.map((p, index) => {
+              {this.props.posts
+                ? this.props.posts.map((p, index) => {
                   return (
                     <div key={p.id}>
                       <PostPreview
@@ -38,8 +42,12 @@ class VisiblePosts extends Component {
                         score={p.voteScore}
                         author={p.author}
                       />
+                      <VoteButton
+                        voteScore={p.voteScore}
+                        id={p.id}
+                      />
                       {/* Add a <hr /> under all but last post */}
-                      {index + 1 !== posts.length ?  <hr /> : null}
+                      {index + 1 !== this.props.posts.length ?  <hr /> : null}
                     </div>
                   );
                 })
@@ -54,8 +62,8 @@ class VisiblePosts extends Component {
 
 }
 const mapStateToProps = (state, { match }) => {
-  const filter = match.params.filter || 'posts';
-  return {
+  let filter = match.params.filter || 'posts';
+    return {
     posts: state.posts,
     filter,
   }
