7c5b32c49bb72619e13b619ed996cb9bedb73116
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-03 20:54:03 -0500

    Read data of single post as an array.

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -9,31 +9,27 @@ class Post extends Component {
   };
 
   render () {
-    const {
-      title,
-      body,
-      author,
-      voteScore,
-      id, } = this.props.posts;
+    console.log('Single Props: ', this.props.posts[0]);
     return (
       <div className="container">
-        {id ?
+        {this.props.posts[0] === undefined ?
+        null
+        :
         <div className="card">
           <div className="card-body">
-            <div className="card-title"><h2>{title}</h2></div>
+            <div className="card-title"><h2>{this.props.posts[0].title}</h2></div>
             <div className="card-text">
-              <p>{body}</p>
+              <p>{this.props.posts[0].body}</p>
             </div>
-            <h6>{`Author: ${author}`}</h6>
+            <h6>{`Author: ${this.props.posts[0].author}`}</h6>
             <VoteButton
-              voteScore={voteScore}
-              id={id}
+              voteScore={this.props.posts[0].voteScore}
+              id={this.props.posts[0].id}
             />
             <button className="btn btn-secondary mt-3">Edit</button>
           </div>
         </div>
-      : null
-      }
+        }
       </div>
     );
   }
