dc46f6109d8546f3751d1e87af1f19dccd7c2c9c
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-05 20:16:16 -0500

    Add route to new_post page.

--- frontend/src/components/body.js
+++ frontend/src/components/body.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../components/home-page';
+import NewPost from '../components/new-post';
 
 const Body = () => (
   <div>
     <Switch>
+      <Route exact path='/create/new_post' component={NewPost} />
       <Route path='/' component={HomePage} />
     </Switch>
   </div>
