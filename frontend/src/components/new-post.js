f809d7774e2acbc162ec1e20734effdb3e9b4fa6
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-05 21:11:39 -0500

    Set up redux connection.

--- frontend/src/components/new-post.js
+++ frontend/src/components/new-post.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      timestamp: '',
+      title: '',
+      body: '',
+      author: '',
+      category: '',
+    }
+  }
+
+  componentDidMount() {
+    this.props.getCategories();
+  }
+
   render () {
     return (
       <div className="container">
@@ -15,13 +33,30 @@ class NewPost extends Component {
                 <label htmlFor="postBody">Post</label>
                 <textarea className="form-control" id="postBody" rows="10"></textarea>
               </div>
+              <div className="form-group">
+                <label htmlFor="postAuthor">Author</label>
+                <input type="text" className="form-control" id="postAuthor"/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="categoryChoice">Category</label>
+                <select className="form-control" id="categoryChoice">
+                  {this.props.categories.map(cat =>
+                    (<option key={cat}>{cat}</option>)
+                    )
+                  }
+                </select>
+              </div>
               <button type="submit" className="btn btn-link">Submit</button>
             </form>
           </div>
         </div>
       </div>
     );
-};
+  };
 }
 
-export default NewPost;
+const mapStateToProps = (state) => ({
+  categories: state.categories,
+})
+
+export default connect(mapStateToProps, actions)(NewPost);
