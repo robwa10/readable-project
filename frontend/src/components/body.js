f9d6bae9d4327c4af265ef81123062228af6e81e
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-08 06:43:40 -0500

    Add route to NewComment.

--- frontend/src/components/body.js
+++ frontend/src/components/body.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../components/home-page';
 import NewPost from '../components/new-post';
+import NewComment from '../containers/new-comment';
 
 const Body = () => (
   <div>
     <Switch>
       <Route exact path='/create/new_post' component={NewPost} />
       <Route exact path='/edit_post/:id' component={NewPost} />
+      <Route exact path='/comment/:id' component={NewComment} />
       <Route path='/' component={HomePage} />
     </Switch>
   </div>
