135a1d0b26afde5864fdb6f44216cdfabf33eb28
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-08 17:41:21 -0500

    Route to EditPost

--- frontend/src/components/body.js
+++ frontend/src/components/body.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../components/home-page';
-import NewPost from '../components/new-post';
+import NewPost from '../containers/new-post';
 import NewComment from '../containers/new-comment';
 import EditComment from '../containers/edit-comment';
+import EditPost from '../containers/edit-post';
 
 const Body = () => (
   <div>
     <Switch>
       <Route exact path='/create/new_post' component={NewPost} />
-      <Route exact path='/edit_post/:id' component={NewPost} />
+      <Route exact path='/edit_post/:id' component={EditPost} />
       <Route exact path='/comment/:id' component={NewComment} />
       <Route exact path='/edit_comment/:id' component={EditComment} />
       <Route path='/' component={HomePage} />
